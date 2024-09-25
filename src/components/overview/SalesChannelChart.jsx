4f259eb11d58e49a7d7f3c2c5d7ff77de7fec1be
thanhphanvan1 <thanhpv27102000@gmail.com>
2024-09-25 21:45:41 +0700

    view-productpage

--- src/components/overview/SalesChannelChart.jsx
+++ src/components/overview/SalesChannelChart.jsx
@@ -1,7 +1,62 @@
 import React from "react";
+import { motion } from "framer-motion";
+import {
+  BarChart,
+  Bar,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  Tooltip,
+  ResponsiveContainer,
+  Legend,
+  Cell,
+} from "recharts";
+const COLORS = ["#6366F1", "#8B5CF6", "#EC4899", "#10B981", "#F59E0B"];
+const SALES_CHANNEL_DATA = [
+  { name: "Website", value: 45600 },
+  { name: "Mobile App", value: 38200 },
+  { name: "Maketplace", value: 29000 },
+  { name: "Social Media", value: 18000 },
+];
 
 const SalesChannelChart = () => {
-  return <div>SalesChannelChart</div>;
+  return (
+    <motion.div
+      className="bg-gray-800 lg:col-span-2 p-6 bg-opacity-50 backdrop-blur-md shadow-lg rounded-xl border border-gray-700"
+      initial={{ opacity: 0, y: 20 }}
+      animate={{ opacity: 1, y: 0 }}
+      transition={{ delay: 0.3 }}
+    >
+      <h2 className="text-lg font-medium mb-4 text-gray-100">
+        Sales by Channel
+      </h2>
+      <div className="h-80">
+        <ResponsiveContainer>
+          <BarChart data={SALES_CHANNEL_DATA}>
+            <CartesianGrid strokeDasharray="3 3" stroke="#4B5563" />
+            <XAxis dataKey="name" stroke="#9CA3AF" />
+            <YAxis stroke="#9CA3AF" />
+            <Tooltip
+              contentStyle={{
+                backgroundColor: "rgba(31,41,55,.8)",
+                borderColor: "#4B5563",
+              }}
+              itemStyle={{ color: "#E5E7EB" }}
+            />
+            <Legend />
+            <Bar dataKey={"value"} fill="#8884d8">
+              {SALES_CHANNEL_DATA.map((entry, index) => (
+                <Cell
+                  key={`cell -${index}`}
+                  fill={COLORS[index % COLORS.length]}
+                />
+              ))}
+            </Bar>
+          </BarChart>
+        </ResponsiveContainer>
+      </div>
+    </motion.div>
+  );
 };
 
 export default SalesChannelChart;
--- src/pages/ProductsPage.jsx
+++ src/pages/ProductsPage.jsx
@@ -1,7 +1,51 @@
 import React from "react";
+import Header from "../components/common/Header";
+import { motion } from "framer-motion";
+import { AlertTriangle, DollarSign, Package, TrendingUp } from "lucide-react";
+import StatCard from "../components/common/StatCard";
 
 const ProductsPage = () => {
-  return <div>ProductsPage</div>;
+  return (
+    <div className="flex-1 overflow-auto relative z-10">
+      <Header title="Products" />
+
+      <main className="max-w-7xl mx-auto py-6 px-4 lg:px-8">
+        <motion.div
+          className="grid grid-cols-1 gap-5 sm:grid-cols-2 lg:grid-cols-4 mb-8"
+          initial={{ opacity: 0, y: 20 }}
+          animate={{ opacity: 1, y: 0 }}
+          transition={{ duration: 1 }}
+        >
+          <StatCard
+            name="Total Products"
+            icon={Package}
+            value="345"
+            color="#6366f1"
+          />
+          <StatCard
+            name="Top Selling"
+            icon={TrendingUp}
+            value="45"
+            color="#6366f1"
+          />
+          <StatCard
+            name="Low Stock"
+            icon={AlertTriangle}
+            value="65"
+            color="#6366f1"
+          />
+          <StatCard
+            name="Total Revenue"
+            icon={DollarSign}
+            value="12500000VND"
+            color="#6366f1"
+          />
+        </motion.div>
+      </main>
+
+      {/* {PRODUCTTABLE} */}
+    </div>
+  );
 };
 
 export default ProductsPage;
